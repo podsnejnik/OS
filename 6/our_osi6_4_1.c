116f86f301a24696207fd4a45f114678c82add1c
Marunya <bezuglaya.marina@gmail.com>
2013-12-26 13:53:19 +0600

    Поправили четвёртую часть шестой задачи. Делала вместе с Мариной Безуглой

--- 6/our_osi6_4_1.c
+++ 6/our_osi6_4_1.c
@@ -5,38 +5,20 @@
 #include <string.h>
 #include <fstream>
 #include <cmath>
-#include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <omp.h>
 
 
 using namespace std;
 
 struct Data {
-	
 	int i = 0;
 	int ** A = new int *[2048];
 	int ** B = new int *[2048];
 	int ** C = new int *[2048];
 };
 
-void *Mult(void *data){
-	Data *local_data = (Data*)data;
-	int Size_M = 2048;
-	int count = (local_data->i)+512;
-	cout<<"count = "<<count<<"\n";
-	int i = local_data->i;
-	cout<<"i = "<<i<<"\n";
-	for (i; i<count; i++){ 
-		for (int j = 0; j < Size_M; j++) {
-		    local_data->C[i][j] = 0;
-		    for (int k = 0; k < Size_M; k++) {
-				local_data->C[i][j] += local_data->A[i][k] * local_data->B[k][j];
-			}
-		}
-	}
-	
-}
 
 void ThreadMatrixMult (Data * data){
 	int Size_M = 2048;
@@ -45,27 +27,31 @@ void ThreadMatrixMult (Data * data){
 		data->C[i]=new int[Size_M];
 	}
 
-	pthread_t thread_ids[4];
-	int thread_error;
-	int thread_num = 0;
 
-	for (data->i = 0; data->i<2048; data->i+=512){
-		cout<<"this is for = "<<data->i<<"\n";
-		thread_error = pthread_create(&thread_ids[thread_num], NULL, Mult, (void *)data);
-		sleep(1);
-		thread_num++;
+	int BlocksNum = 2;
+	int ThreadNum = 4;
+	omp_set_num_threads(ThreadNum);
+	#pragma omp parallel
+	{
+		int ThreadID = omp_get_thread_num();
+		int RowIndex = ThreadID/BlocksNum;
+		int ColIndex = ThreadID%BlocksNum;
+		int BlockSize = 2048/BlocksNum;
+		for (int i=0; i<BlockSize; i++){
+			for (int j=0; j<BlockSize; j++){
+				for (int k=0; k<2048; k++){
+					data->C[(RowIndex*BlockSize+i)][(ColIndex*BlockSize+j)] += data->A[(RowIndex*BlockSize+i)][k] * data->B[k][(ColIndex*BlockSize+j)];
+				}
+			}
+		}
 	}
+	
 
 
-    for (int j = 0; j<4; j++){
-        pthread_join(thread_ids[j], NULL);
-    }
-
-    
-
-    // for (int i=0; i<2048; i++){
-    // 	for (int j=0; j<2048; j++){
-    // 		cout<<data->C[i][j]<<" ";
+	
+    // for (int i1=0; i1<2048; i1++){
+    // 	for (int j1=0; j1<2048; j1++){
+    // 		cout<<data->C[i1][j1]<<" ";
     // 	}
     // 	cout<<"\n";
     // }
